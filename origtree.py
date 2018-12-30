6fe5671f81d31cafaf10fe59668984d08b1851cd
anmaxwell <aniamaxwell@gmail.com>
2018-12-30 08:18:13 +0000

    trying to get the loop of reading the nodes

--- origtree.py
+++ origtree.py
@@ -39,57 +39,59 @@ def printChildren(node, counter):
 def readChild(initinput):
 
     inputlist = initinput.split()
-    leninputlist = len(inputlist)
-    nodename = 0
     
-    while leninputlist >0:
+    anode = Node('')
+    firstchar = int(inputlist[0])
+    secchar = int(inputlist[1])
+    inputlist.pop(0)
+    inputlist.pop(0)
     
-        anode = Node(nodename)
-        nodename+=1
-        firstchar = int(inputlist[0])
-        secchar = int(inputlist[1])
-        print(firstchar, secchar, inputlist)
-        inputlist.pop(0)
-        inputlist.pop(0)
-
-        
-        if firstchar==0:
-            for j in range(secchar):
-                anode.addMeta(inputlist[0])
-                inputlist.pop(0)
-        else:        
-            #read meta
-            for j in range(secchar):
-                anode.addMeta(inputlist[-1])
-                inputlist.pop()
-      
-            #read children for this anode
-            
-            
-            #anode.addChild(child)
-            
-        
+    if firstchar==0:
+        for j in range(secchar):
+            # this was added prior to moving the pop command above so may no
+            # longer work!!!!!
+            anode.addMeta(inputlist[2])
+            inputlist.pop(2)
+    else:        
+        #read meta
+        for j in range(secchar):
+            anode.addMeta(inputlist[-1])
+            inputlist.pop()
+  
+        #read children for this anode  
+        #anode.addChild(child)        
 
     #print(inputlist)
 
-#    nn = Node("aChil")
-#    nn.addMeta(666)
-#
-#    ii = Node("anotherChil")
-#    ii.addMeta(9)
-#    ii.addMeta(99)
-#    
-#    yy = Node("ThirdLayer")
-#    yy.addMeta(7)
-#    ii.addChild(yy)
-#
-#    anode.addChild(nn)
-#    anode.addChild(ii)
+    nn = Node("aChil")
+    nn.addMeta(666)
+
+    ii = Node("anotherChil")
+    ii.addMeta(9)
+    ii.addMeta(99)
+    
+    yy = Node("ThirdLayer")
+    yy.addMeta(7)
+    ii.addChild(yy)
+
+
+    anode.addChild(nn)
+    anode.addChild(ii)
+
     
 #    print(anode.children[0])
     print()
     print("Root:", anode)
-    printChildren(anode, 1)        
+    printChildren(anode, 1)
+    
+#    for i in range(len(anode.children)):
+#        print("  Child:", anode.children[i])
+#        childNode = anode.children[i]
+#        
+#        if len(childNode.children):
+#            for y in range(len(childNode.children)):
+#                print("     Childs child:", childNode.children[y])
+        
     print()
     return anode
         
